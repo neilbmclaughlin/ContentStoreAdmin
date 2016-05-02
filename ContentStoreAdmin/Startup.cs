171e8295f873b7ade00d7f630af94e8c07c33f06
Neil <Neil@SANTA-CRUZ>
2016-05-02 22:57:01 +0100

    Add project files.

---
+++ ContentStoreAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContentStoreAdmin.Startup))]
+namespace ContentStoreAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
