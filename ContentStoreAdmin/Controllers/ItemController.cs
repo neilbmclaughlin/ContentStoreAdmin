2cbba88672fe6b102543b76fa33f9d2014ecb625
Neil McLaughlin <Neil McLaughlin>
2016-05-03 00:20:42 +0100

    Added basic setup for DocumentDB backed MVC app. Does not seem to work at the moment.

---
+++ ContentStoreAdmin/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ContentStoreAdmin.Models;
+
+namespace ContentStoreAdmin.Controllers
+{
+    public class ItemController : Controller
+    {
+    [ActionName("Index")]
+    public async Task<ActionResult> IndexAsync()
+    {
+      var items = await DocumentDbRepository<Item>.GetItemsAsync(d => !d.Completed);
+      return View(items);
+    }
+  }
+}
